c5f2297cfb77f7d2d0fd177c5ddd361dc2e499c4
CPkobo <sheepy.workshop@gmail.com>
2023-03-17 16:51:22 +0900

    index page added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
     typescript: {
         shim: false,
     },
-        build: {
+    build: {
         transpile: ["primevue"],
     },
     css: [
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <ul>
+        <li><NuxtLink to="app">類似漢字チェッカー</NuxtLink></li>
+    </ul>
+</template>
